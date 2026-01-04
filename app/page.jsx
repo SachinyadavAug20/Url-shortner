d896bca5330bf67ed3f195ea21ff15facae63255
SachinyadavAug20 <samtagon777@gmail.com>
2026-01-04 21:10:19 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <div className="text-center w-full mx-auto p-2 text-2xl font-bold bg-black text-white">
+                Hii URL shortner
+            </div>
+        </>
+
+    );
+}
